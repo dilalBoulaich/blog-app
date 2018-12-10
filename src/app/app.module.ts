5ebb752aaa3f0deb0e175f4b32971c1e36117d4f
Boulaich Ayoub <ayoub@nearsoft.pro>
2018-12-10 11:04:00 +0100

    Redirect Routing Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
@@ -11,7 +11,17 @@ import { FormsModule } from '@angular/forms';
 
 const appRoutes: Routes = [
   { path: 'posts', component: PostListComponent },
-  { path: 'new', component: NewPostComponent }
+  { path: 'new', component: NewPostComponent} ,
+  
+  {
+    path: '',
+    redirectTo: '/posts',
+    pathMatch: 'full'
+  },
+  { path: '**',
+    redirectTo: '/posts',
+    pathMatch: 'full'  
+  }
 ];
 
 @NgModule({
